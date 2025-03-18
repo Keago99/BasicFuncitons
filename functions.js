0e74515718a16b807a052a90d88bff131140f342
Keago99 <keatabde@gmail.com>
2025-03-18 22:04:30 +0200

    complete 2 tasks, learned how to use substring method. good practice.

--- functions.js
+++ functions.js
@@ -3,4 +3,22 @@ function add7(number){
     console.log(newNumber);
 }
 
+function multiply(number1, number2){
+    let newNumber  = number1 * number2;
+    console.log(newNumber);
+}
+
+function capitalize(string){
+    let newUpper = string[0].toUpperCase();
+    let newWord = (newUpper + string.substring(1));
+    console.log(newWord);
+}
+
+
 add7(400);
+multiply(4,10);
+capitalize("fart");
+capitalize("FART");
+capitalize("fArT");
+
+
